2ed951771a329cb73e92ee38a0123466fea0c9e6
Bui The Anh <anh.bui@axonactive.com>
2019-07-23 16:16:53 +0700

    Separate multiple target: Production, Stating, Dev

--- SwiftSourceBase/SwiftSourceBase/ViewController.swift
+++ SwiftSourceBase/SwiftSourceBase/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var serverLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        displayServer()
     }
 
+    private func displayServer() {
+        #if DEV
+            serverLabel.text = "This is DEV server"
+        #elseif STG
+            serverLabel.text = "This is STAGING server"
+        #else
+            serverLabel.text = "This is PRODUCTION server"
+        #endif
+    }
 
 }
 
